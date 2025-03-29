816ec30f12e03adbae9bedecbbfef0c4ae036cb7
Md Abul Kalam <tipu4142@gmail.com>
2025-03-30 01:19:05 +0600

    recipe pagination and using virtual dom modify text and attribute without rendering the whole dom

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/';
 export const TIMEOUT_SEC = 10;
 export const API_KEY = '7f947b40-ab13-4837-b4ee-5abff8cf95c';
+export const RESULTS_PER_PAGE = 10;
